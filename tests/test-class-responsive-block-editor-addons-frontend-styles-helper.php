dbcba5074b6c851a4a7c556f5b2e217dbb7e8891
Smriti Chaturvedi <smriti.chaturvedi@hbwsl.com>
2021-07-12 14:31:08 +0530

    RBEA-469: Wrote unit test cases for default attributes for frontend styles

--- tests/test-class-responsive-block-editor-addons-frontend-styles-helper.php
+++ tests/test-class-responsive-block-editor-addons-frontend-styles-helper.php
@@ -37,6 +37,8 @@ class Responsive_Block_Editor_Addons_Frontend_Styles_Helper_Test extends WP_Unit
 	public function test_responsive_block_editor_addons_frontend_styles_helper_constructor() {
 		self::$rbea_frontend_styles_helper = new Responsive_Block_Editor_Addons_Frontend_Styles_Helper();
 		$this->assertTrue( self::$rbea_frontend_styles_helper instanceof Responsive_Block_Editor_Addons_Frontend_Styles_Helper );
+		$this->assertEquals( 100, has_action( 'wp_head', array( self::$rbea_frontend_styles_helper, 'responsive_block_editor_addons_description' ) ) );
+		$this->assertEquals( 100, has_action( 'wp_head', array( self::$rbea_frontend_styles_helper, 'responsive_block_editor_addons_frontend_styles' ) ) );
 	}
 
 	/**
@@ -46,5 +48,13 @@ class Responsive_Block_Editor_Addons_Frontend_Styles_Helper_Test extends WP_Unit
 		$expected_output = "\n<!-- This block is generated with the Responsive Blocks Library Plugin v" . substr( RESPONSIVE_BLOCK_EDITOR_ADDONS_VER, 0, -2 ) . ' (Responsive Gutenberg Blocks Library ' . RESPONSIVE_BLOCK_EDITOR_ADDONS_VER . ") - https://cyberchimps.com/blocks/ -->\n\n";
 		$this->expectOutputString( $expected_output );
 		self::$rbea_frontend_styles_helper->responsive_block_editor_addons_description();
+	}
+
+	/**
+	 * Test for get_instance function
+	 */
+	public function test_get_instance() {
+		$instance = self::$rbea_frontend_styles_helper->get_instance();
+		$this->assertEquals( self::$rbea_frontend_styles_helper, $instance );
 	}
 }
