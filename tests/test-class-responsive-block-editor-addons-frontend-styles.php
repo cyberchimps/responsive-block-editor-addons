8da58d01cbd35301490f32dcb5babe767c4eefb6
Smriti Chaturvedi <smriti.chaturvedi@hbwsl.com>
2021-07-12 11:30:18 +0530

    RBEA-469: Added unit test cases for frontend styles

--- tests/test-class-responsive-block-editor-addons-frontend-styles.php
+++ tests/test-class-responsive-block-editor-addons-frontend-styles.php
@@ -84,4 +84,93 @@ class Responsive_Block_Editor_Addons_Frontend_Styles_Test extends WP_UnitTestCas
 		$this->assertTrue( method_exists( 'Responsive_Block_Editor_Addons_Frontend_Styles', 'get_css_value' ) );
 		$this->assertTrue( method_exists( 'Responsive_Block_Editor_Addons_Frontend_Styles', 'generate_background_image_effect' ) );
 	}
+
+	/**
+	 * Test the values for default attributes for call to action block
+	 */
+	public function test_responsive_block_advanced_heading_default_attributes() {
+		$expected_values = array(
+			'block_id'                      => '',
+			'headingTitle'                  => '',
+			'headingDesc'                   => '',
+			'seperatorStyle'                => 'solid',
+			'separatorWidthType'            => '%',
+			'separatorColor'                => '',
+			'headingTitleColor'             => '',
+			'subHeadingTitleColor'          => '',
+			'headingTitleFontFamily'        => '',
+			'subHeadingTitleFontFamily'     => '',
+			'headingTitleFontSize'          => '',
+			'subHeadingTitleFontSize'       => '',
+			'headingTitleFontSizeTablet'    => '',
+			'subHeadingTitleFontSizeTablet' => '',
+			'headingTitleFontSizeMobile'    => '',
+			'subHeadingTitleFontSizeMobile' => '',
+			'headingTitleFontWeight'        => '600',
+			'subHeadingTitleFontWeight'     => '400',
+			'headingTag'                    => 'h2',
+			'headingAlignment'              => 'center',
+			'headingAlignmentTablet'        => 'center',
+			'headingAlignmentMobile'        => 'center',
+			'showHeading'                   => true,
+			'showSubHeading'                => true,
+			'showSeparator'                 => true,
+			'separatorHeight'               => 3,
+			'separatorWidth'                => 20,
+			'headSpacing'                   => 15,
+			'subheadSpacing'                => 15,
+			'separatorSpacing'              => 15,
+			'headSpacingTablet'             => 15,
+			'subheadSpacingTablet'          => 15,
+			'separatorSpacingTablet'        => 15,
+			'headSpacingMobile'             => 15,
+			'subheadSpacingMobile'          => 15,
+			'separatorSpacingMobile'        => 15,
+			'headingTitleLineHeight'        => 1,
+			'subHeadingTitleLineHeight'     => 1,
+			'headingTitleLetterSpacing'     => 0,
+			'subHeadingTitleLetterSpacing'  => 0,
+			'level'                         => 2,
+			'textDecoration'                => 'none',
+			'textDecorationSubHeading'      => 'none',
+		);
+		$values          = self::$rbea_frontend_styles->get_responsive_block_advanced_heading_default_attributes();
+		$this->assertEmpty( array_diff_assoc( $expected_values, $values ) );
+	}
+
+	/**
+	 * Test the values for default attributes for expand block
+	 */
+	public function test_responsive_block_expand_default_attributes() {
+		$expected_values = array(
+			'block_id'        => '',
+			'blockTitle'      => 'Title for this block',
+			'expandLessText'  => 'Some short text that can be expanded to show more details.',
+			'expandMoreText'  => 'Some short text that can be expanded to show more details. Description for this block. Use this space for describing your block. Any text will do. Description for this block. You can use this space for describing your block.',
+			'moreLabel'       => 'Show more',
+			'lessLabel'       => 'Show less',
+			'showTitle'       => true,
+			'expandAlignment' => '',
+			'textColor'       => '',
+			'linkColor'       => '',
+			'titleColor'      => '',
+			'titleSpace'      => 28,
+			'textSpace'       => 20,
+			'linkSpace'       => 18,
+			'titleFontFamily' => '',
+			'textFontFamily'  => '',
+			'linkFontFamily'  => '',
+			'titleFontSize'   => 20,
+			'titleFontWeight' => 400,
+			'titleLineHeight' => 1,
+			'textFontSize'    => 16,
+			'textFontWeight'  => 400,
+			'textLineHeight'  => 2,
+			'linkFontSize'    => 16,
+			'linkFontWeight'  => 400,
+			'linkLineHeight'  => 1,
+		);
+		$values          = self::$rbea_frontend_styles->get_responsive_block_expand_default_attributes();
+		$this->assertEmpty( array_diff_assoc( $expected_values, $values ) );
+	}
 }
