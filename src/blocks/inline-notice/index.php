945cafc6552c724fe120d5459b10f0d50454980b
vibhav salelkar <vibhav.salelkar@hbwsl.com>
2021-07-29 17:56:28 +0530

    inline-notice: index.php phpcbf

--- src/blocks/inline-notice/index.php
+++ src/blocks/inline-notice/index.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Server-side rendering for the inline notice block
+ *
+ * @since   1.0.0
+ * @package Responsive Blocks
+ */
 
 /**
  * Add Frontend assets.
@@ -7,7 +13,7 @@
  */
 function responsive_block_editor_addons_inline_notice_add_frontend_assets( $attributes ) {
 	if ( has_block( 'responsive-block-editor-addons/inline-notice' ) ) {
-		wp_enqueue_script(
+		wp_enqueue_script(//phpcs:ignore
 			'responsive_block_editor_addons-inline-notice-cookies-script',
 			RESPONSIVE_BLOCK_EDITOR_ADDONS_URL . '/dist/js/vendors/js_cookie.js',
 			array(),
