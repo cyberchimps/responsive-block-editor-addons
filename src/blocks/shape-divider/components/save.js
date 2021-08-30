d9507afe2785356886d311230b2c38d47269ba2a
Nayan <nayan.gaikwad@hbwsl.com>
2021-08-30 12:19:18 +0530

    Shape Divider - Shapes should be visible when block is added, also move Styles panel inside tabs

--- src/blocks/shape-divider/components/save.js
+++ src/blocks/shape-divider/components/save.js
@@ -22,6 +22,7 @@ const save = ({ attributes, className }) => {
     horizontalFlip,
     verticalFlip,
     align,
+      design,
   } = attributes;
 
   const shapeClass = getColorClassName("color", color);
@@ -34,7 +35,8 @@ const save = ({ attributes, className }) => {
     className,
     "responsive-block-editor-addons-block-shape-divider",
     `block-${block_id}`,
-    {
+      `is-rbea-separator-style-${design}`,
+      {
       "is-vertically-flipped": verticalFlip,
       "is-horizontally-flipped": horizontalFlip,
       [shapeClass]: shapeClass,
@@ -56,7 +58,7 @@ const save = ({ attributes, className }) => {
   return (
     <div className={classes} aria-hidden="true">
       <div className="wp-block-responsive-block-editor-addons-shape-divider__svg-wrapper">
-        {getDividerFromStyle(attributes.className)}
+        {getDividerFromStyle(classes)}
       </div>
       <div className="wp-block-responsive-block-editor-addons-shape-divider__alt-wrapper"></div>
     </div>
