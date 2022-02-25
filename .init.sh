b81c41118e968f7de695126e6d80be179a12918a
ratnesh-kadam <ratnesh.kadam@hbwsl.com>
2022-02-25 10:34:48 +0530

    Update .init.sh

--- .init.sh
+++ .init.sh
@@ -5,6 +5,6 @@ wp rewrite structure '/%year%/%monthnum%/%postname%/'
 wp plugin delete hello
 wp plugin delete akismet
 
-wp plugin activate responsive-block-editor-addons
+wp plugin activate ${REPO_NAME}
 
 # wp plugin install plugin_slug --activate
