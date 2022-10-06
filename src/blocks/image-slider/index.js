e8fdf0c0c4cb90b61c1a7de0d61f85209bff3fec
tejas <liktejas@gmail.com>
2022-10-06 17:07:32 +0530

    Fix-additional-css-classes-issue

--- src/blocks/image-slider/index.js
+++ src/blocks/image-slider/index.js
@@ -3,7 +3,7 @@
  */
 import edit from "./components/edit";
 import metadata from "./block.json";
-import save from "./components/save";
+import Save from "./components/save";
 
 //Import Block icon
 import ResponsiveBlockEditorAddonsIcons from "../../block-icons";
@@ -59,5 +59,8 @@ registerBlockType("responsive-block-editor-addons/image-slider", {
     },
   },
   edit,
-  save,
+  // save,
+  save: (props) => {
+    return <Save {...props} />;
+  },
 });
