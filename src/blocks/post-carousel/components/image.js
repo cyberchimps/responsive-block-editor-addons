b1039271ac7210ae1afee9566f60735e51af73ef
Pratik Raja <>
2023-12-21 17:12:56 +0530

    fixed err on hovering over portfolio block

--- src/blocks/post-carousel/components/image.js
+++ src/blocks/post-carousel/components/image.js
@@ -41,7 +41,9 @@ export default class PostCarouselImage extends Component {
     /**
      * Cancel the image URL subscription.
      */
-    this.state.setImageUrlSubscription();
+    if(typeof this.state.setImageUrlSubscription==='function'){
+      this.state.setImageUrlSubscription();
+    }
   }
 
   setImageUrl() {
