658b31a1379a716c3d78ab85b8172252319469a8
Pratik Raja <>
2024-03-05 11:15:41 +0530

    changed social share block name to social icon

--- src/blocks/social-share/index.js
+++ src/blocks/social-share/index.js
@@ -22,7 +22,7 @@ const { registerBlockType } = wp.blocks;
 
 // Register the block
 registerBlockType("responsive-block-editor-addons/social-share", {
-  title: __("Social Share", "responsive-block-editor-addons"),
+  title: __("Social Icons", "responsive-block-editor-addons"),
   description: __(
     "This block allows you to display icons linking to your social media profiles or websites",
     "responsive-block-editor-addons"
@@ -30,7 +30,7 @@ registerBlockType("responsive-block-editor-addons/social-share", {
   icon: ResponsiveBlockEditorAddonsIcons.social_share,
   category: "responsive_block_editor_addons",
   keywords: [
-    __("Social Share", "responsive-block-editor-addons"),
+    __("Social Icons", "responsive-block-editor-addons"),
     __("social icons", "responsive-block-editor-addons"),
     __("Responsive", "responsive-block-editor-addons"),
   ],
