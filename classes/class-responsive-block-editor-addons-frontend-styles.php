48fba5e2ccf1bac85d0efcfe6830ed5eba54f4bb
Tejas Sonawane <tejas.sonawane@hbwsl.com>
2025-07-24 18:21:41 +0530

    Implement shadow settings for pricing table block

--- classes/class-responsive-block-editor-addons-frontend-styles.php
+++ classes/class-responsive-block-editor-addons-frontend-styles.php
@@ -10054,9 +10054,13 @@ if ( ! class_exists( 'Responsive_Block_Editor_Addons_Frontend_Styles' ) ) {
 			$mobile_selectors = array();
 			$tablet_selectors = array();
 
-			$box_shadow_position_css = $attr['boxShadowPosition'];
+			$box_shadow_position_css       = $attr['boxShadowPosition'];
+			$hover_box_shadow_position_css = $attr['hoverboxShadowPosition'];
 			if ( 'outset' === $attr['boxShadowPosition'] ) {
 				$box_shadow_position_css = '';
+			}
+			if ( 'outset' === $attr['hoverboxShadowPosition'] ) {
+				$hover_box_shadow_position_css = '';
 			}
 
 			$button_box_shadow_position_css = $attr['buttonBoxShadowPosition'];
@@ -10223,6 +10227,20 @@ if ( ! class_exists( 'Responsive_Block_Editor_Addons_Frontend_Styles' ) ) {
 						$attr['boxShadowColor'] .
 						' ' .
 						$box_shadow_position_css,
+				),
+				' .wp-block-responsive-block-editor-addons-pricing-table-item:hover' => array(
+					'box-shadow'                 =>
+						self::get_css_value( $attr['hoverboxShadowHOffset'], 'px' ) .
+						' ' .
+						self::get_css_value( $attr['hoverboxShadowVOffset'], 'px' ) .
+						' ' .
+						self::get_css_value( $attr['hoverboxShadowBlur'], 'px' ) .
+						' ' .
+						self::get_css_value( $attr['hoverboxShadowSpread'], 'px' ) .
+						' ' .
+						$attr['hoverboxShadowColor'] .
+						' ' .
+						$hover_box_shadow_position_css,
 				),
 
 				' .wp-block-responsive-block-editor-addons-pricing-table-item__title' => array(
@@ -10476,6 +10494,12 @@ if ( ! class_exists( 'Responsive_Block_Editor_Addons_Frontend_Styles' ) ) {
 				'boxShadowBlur'                => 0,
 				'boxShadowSpread'              => 0,
 				'boxShadowPosition'            => 'outset',
+				'hoverboxShadowColor'          => '',
+				'hoverboxShadowHOffset'        => 0,
+				'hoverboxShadowVOffset'        => 0,
+				'hoverboxShadowBlur'           => 0,
+				'hoverboxShadowSpread'         => 0,
+				'hoverboxShadowPosition'       => 'outset',
 				'buttonBoxShadowColor'         => '',
 				'buttonBoxShadowHOffset'       => 0,
 				'buttonBoxShadowVOffset'       => 0,
