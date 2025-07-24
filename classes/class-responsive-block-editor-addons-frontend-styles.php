e1c2873bbda59eda520c62fc375cff7d61f760c8
Tejas Sonawane <tejas.sonawane@hbwsl.com>
2025-07-24 17:29:48 +0530

    Implement shadow settings for image boxes block

--- classes/class-responsive-block-editor-addons-frontend-styles.php
+++ classes/class-responsive-block-editor-addons-frontend-styles.php
@@ -7484,10 +7484,14 @@ if ( ! class_exists( 'Responsive_Block_Editor_Addons_Frontend_Styles' ) ) {
 			) .
 			')';
 
-			$box_shadow_position_css = $attr['boxShadowPosition'];
+			$box_shadow_position_css       = $attr['boxShadowPosition'];
+			$hover_box_shadow_position_css = $attr['hoverboxShadowPosition'];
 
 			if ( 'outset' === $attr['boxShadowPosition'] ) {
 				$box_shadow_position_css = '';
+			}
+			if ( 'outset' === $attr['hoverboxShadowPosition'] ) {
+				$hover_box_shadow_position_css = '';
 			}
 
 			$background_image_first  = '';
@@ -7613,6 +7617,18 @@ if ( ! class_exists( 'Responsive_Block_Editor_Addons_Frontend_Styles' ) ) {
 
 				':hover'           => array(
 					'transform' => 'scale(' . $attr['imageHoverEffect'] . ')',
+					'box-shadow'                 =>
+						self::get_css_value( $attr['hoverboxShadowHOffset'], 'px' ) .
+						' ' .
+						self::get_css_value( $attr['hoverboxShadowVOffset'], 'px' ) .
+						' ' .
+						self::get_css_value( $attr['hoverboxShadowBlur'], 'px' ) .
+						' ' .
+						self::get_css_value( $attr['hoverboxShadowSpread'], 'px' ) .
+						' ' .
+						$attr['hoverboxShadowColor'] .
+						' ' .
+						$hover_box_shadow_position_css,
 				),
 
 				' .responsive-block-editor-addons-imagebox-image' => array(
@@ -7813,13 +7829,18 @@ if ( ! class_exists( 'Responsive_Block_Editor_Addons_Frontend_Styles' ) ) {
 				'arrowColor'                     => '',
 				'arrowSize'                      => '',
 				'boxShadowColor'                 => '#fff',
-				'boxShadowHOffset'               => 9,
-				'boxShadowVOffset'               => 9,
-				'boxShadowBlur'                  => 9,
+				'boxShadowHOffset'               => 0,
+				'boxShadowVOffset'               => 0,
+				'boxShadowBlur'                  => 0,
 				'opacity'                        => 70,
 				'hoverOpacity'                   => 70,
-				'boxShadowSpread'                => 9,
-				'boxShadowPosition'              => 'outset',
+				'boxShadowSpread'                => 0,
+				'hoverboxShadowColor'            => '#fff',
+				'hoverboxShadowHOffset'          => 0,
+				'hoverboxShadowVOffset'          => 0,
+				'hoverboxShadowBlur'             => 0,
+				'hoverboxShadowSpread'           => 0,
+				'hoverboxShadowPosition'         => 'outset',
 				'backgroundPosition'             => '',
 				'backgroundSize'                 => '',
 				'backgroundRepeat'               => '',
