cddd3769b0d11b0bf5619f3b9243529c337f7a90
Tejas Sonawane <tejas.sonawane@hbwsl.com>
2025-09-04 16:24:03 +0530

    Fix box shadow issue in cta

--- classes/class-responsive-block-editor-addons-frontend-styles.php
+++ classes/class-responsive-block-editor-addons-frontend-styles.php
@@ -4236,7 +4236,7 @@ if ( ! class_exists( 'Responsive_Block_Editor_Addons_Frontend_Styles' ) ) {
 					'margin-left'                => self::get_css_value( $attr['blockLeftMargin'], 'px' ),
 				),
 				':hover' => array(
-					'box-shadow'                 =>
+					'box-shadow' => ( isset( $attr['hoverboxShadowColor'] ) && ! empty( $attr['hoverboxShadowColor'] ) ) ?
 						self::get_css_value( $attr['hoverboxShadowHOffset'], 'px' ) .
 						' ' .
 						self::get_css_value( $attr['hoverboxShadowVOffset'], 'px' ) .
@@ -4247,7 +4247,7 @@ if ( ! class_exists( 'Responsive_Block_Editor_Addons_Frontend_Styles' ) ) {
 						' ' .
 						$attr['hoverboxShadowColor'] .
 						' ' .
-						$hover_box_shadow_position_css,
+						$hover_box_shadow_position_css : '',
 				),
 
 				' .responsive-block-editor-addons-cta-image' => array(
