8747884b3929905d3c9d97a35f926ff9ec183a01
Tejas Sonawane <tejas.sonawane@hbwsl.com>
2025-09-04 17:18:55 +0530

    Fix the default value issue in image box

--- classes/class-responsive-block-editor-addons-frontend-styles.php
+++ classes/class-responsive-block-editor-addons-frontend-styles.php
@@ -8226,10 +8226,11 @@ if ( ! class_exists( 'Responsive_Block_Editor_Addons_Frontend_Styles' ) ) {
 				'boxShadowHOffset'               => 0,
 				'boxShadowVOffset'               => 0,
 				'boxShadowBlur'                  => 0,
+				'boxShadowPosition'              => 'outset',
 				'opacity'                        => 70,
 				'hoverOpacity'                   => 70,
 				'boxShadowSpread'                => 0,
-				'hoverboxShadowColor'            => '#fff',
+				'hoverboxShadowColor'            => '',
 				'hoverboxShadowHOffset'          => 0,
 				'hoverboxShadowVOffset'          => 0,
 				'hoverboxShadowBlur'             => 0,
@@ -10636,7 +10637,7 @@ if ( ! class_exists( 'Responsive_Block_Editor_Addons_Frontend_Styles' ) ) {
 						$box_shadow_position_css,
 				),
 				' .wp-block-responsive-block-editor-addons-pricing-table-item:hover' => array(
-					'box-shadow'                 =>
+					'box-shadow'                 => ( isset( $attr['hoverboxShadowColor'] ) && ! empty( $attr['hoverboxShadowColor'] ) ) ?
 						self::get_css_value( $attr['hoverboxShadowHOffset'], 'px' ) .
 						' ' .
 						self::get_css_value( $attr['hoverboxShadowVOffset'], 'px' ) .
@@ -10647,7 +10648,7 @@ if ( ! class_exists( 'Responsive_Block_Editor_Addons_Frontend_Styles' ) ) {
 						' ' .
 						$attr['hoverboxShadowColor'] .
 						' ' .
-						$hover_box_shadow_position_css,
+						$hover_box_shadow_position_css : '',
 				),
 
 				' .wp-block-responsive-block-editor-addons-pricing-table-item__title' => array(
