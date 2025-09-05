cfa20fbdce267a8d420b80efa432898252a2c32d
Tejas Sonawane <tejas.sonawane@hbwsl.com>
2025-09-05 14:12:46 +0530

    Fix the box-shadow issues for blockquote, info and post grid blocks

--- src/blocks/post-grid/index.php
+++ src/blocks/post-grid/index.php
@@ -977,19 +977,19 @@ function responsive_block_editor_addons_register_block_core_latest_posts() {
 				),
 				'boxShadowHOffset'            => array(
 					'type'    => 'number',
-					'default' => '0',
+					'default' => 0,
 				),
 				'boxShadowVOffset'            => array(
 					'type'    => 'number',
-					'default' => '0',
+					'default' => 0,
 				),
 				'boxShadowBlur'               => array(
 					'type'    => 'number',
-					'default' => '0',
+					'default' => 0,
 				),
 				'boxShadowSpread'             => array(
 					'type'    => 'number',
-					'default' => '0',
+					'default' => 0,
 				),
 				'boxShadowPosition'           => array(
 					'type'    => 'string',
@@ -1000,19 +1000,19 @@ function responsive_block_editor_addons_register_block_core_latest_posts() {
 				),
 				'hoverboxShadowHOffset'       => array(
 					'type'    => 'number',
-					'default' => '0',
+					'default' => 0,
 				),
 				'hoverboxShadowVOffset'       => array(
 					'type'    => 'number',
-					'default' => '0',
+					'default' => 0,
 				),
 				'hoverboxShadowBlur'          => array(
 					'type'    => 'number',
-					'default' => '0',
+					'default' => 0,
 				),
 				'hoverboxShadowSpread'        => array(
 					'type'    => 'number',
-					'default' => '0',
+					'default' => 0,
 				),
 				'hoverboxShadowPosition'      => array(
 					'type'    => 'string',
