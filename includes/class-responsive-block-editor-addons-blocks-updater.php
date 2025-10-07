7b1b27ee2a7e3b48749efde028fb9c15cf568d35
Tejas Sonawane <tejas.sonawane@hbwsl.com>
2025-10-07 17:30:20 +0530

    Fix display condition doc link in updater list

--- includes/class-responsive-block-editor-addons-blocks-updater.php
+++ includes/class-responsive-block-editor-addons-blocks-updater.php
@@ -536,7 +536,7 @@ class Responsive_Block_Editor_Addons_Blocks_Updater {
 				'data'     => array(
 					'key'      => 'display-conditions',
 					'title'    => 'Display Conditions',
-					'docs'     => 'https://cyberchimps.com/docs/responsive-blocks/blocks/display-conditions/',
+					'docs'     => 'https://cyberchimps.com/docs/responsive-blocks/general/display-conditions-extension/',
 					'demo'     => 'https://cyberchimps.com/responsive-blocks/display-conditions/',
 					'category' => 'extensions',
 					'status'   => 1,
