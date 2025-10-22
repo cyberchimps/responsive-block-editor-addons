90acfdbf97b5cfa7310615274596df1a19d6010b
Nikhil <sharma842nikhil@gmail.com>
2025-10-22 10:03:16 +0530

    remove unnecessary code

--- src/blocks/contact-form-7-styler/attributes.js
+++ src/blocks/contact-form-7-styler/attributes.js
@@ -1776,18 +1776,6 @@ const attributes = {
     type: "string",
     default: '',
   },
-  inheritFromTheme: {
-    type: "boolean",
-    default: (typeof responsive_globals !== 'undefined' && responsive_globals.global_inherit_from_theme === '1'),
-  },
-  inheritFromThemesaved: {
-    type: "boolean",
-    default: false,
-  },
-  inheritFromThemeLocalTimestamp: {
-    type: "string",
-    default: "",
-  },
 };
 
 export default attributes;
