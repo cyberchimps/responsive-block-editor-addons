52594de9ce84a3710f3ddf533452d1fa2b49dd08
Nikhil <sharma842nikhil@gmail.com>
2025-11-21 14:57:03 +0530

    modification in styling of feature grid for gradient color picker changes

--- classes/class-responsive-block-editor-addons-frontend-styles.php
+++ classes/class-responsive-block-editor-addons-frontend-styles.php
@@ -20052,7 +20052,7 @@ if ( ! class_exists( 'Responsive_Block_Editor_Addons_Frontend_Styles' ) ) {
 			if ( 'color' === $attr['buttonHbackgroundType'] ) {
 				$updated_button_bg_h_color = $attr['ctaHoverBackColor'];
 			} elseif ( 'gradient' === $attr['buttonHbackgroundType'] ) {
-				$updated_button_bg_h_image = 'linear-gradient(' . $attr['buttonHgradientDirection'] . 'deg, ' . $attr['buttonHbackgroundColor1'] . ' ' . $attr['buttonHcolorLocation1'] . '%, ' . $attr['buttonHbackgroundColor2'] . ' ' . $attr['buttonHcolorLocation2'] . '%)';
+				$updated_button_bg_h_image = $attr['gradientButtonH'] ? $attr['gradientButtonH'] : 'linear-gradient(' . $attr['buttonHgradientDirection'] . 'deg, ' . $attr['buttonHbackgroundColor1'] . ' ' . $attr['buttonHcolorLocation1'] . '%, ' . $attr['buttonHbackgroundColor2'] . ' ' . $attr['buttonHcolorLocation2'] . '%)';
 			}
 
 			$updated_button_background_color = '';
@@ -20060,7 +20060,7 @@ if ( ! class_exists( 'Responsive_Block_Editor_Addons_Frontend_Styles' ) ) {
 			if ( 'color' === $attr['buttonbackgroundType'] ) {
 				$updated_button_background_color = $attr['ctaBackColor'];
 			} elseif ( 'gradient' === $attr['buttonbackgroundType'] ) {
-				$updated_button_background_image = 'linear-gradient(' . $attr['buttongradientDirection'] . 'deg, ' . $attr['buttonbackgroundColor1'] . ' ' . $attr['buttoncolorLocation1'] . '%, ' . $attr['buttonbackgroundColor2'] . ' ' . $attr['buttoncolorLocation2'] . '%)';
+				$updated_button_background_image = $attr['gradientButton'] ? $attr['gradientButton'] : 'linear-gradient(' . $attr['buttongradientDirection'] . 'deg, ' . $attr['buttonbackgroundColor1'] . ' ' . $attr['buttoncolorLocation1'] . '%, ' . $attr['buttonbackgroundColor2'] . ' ' . $attr['buttoncolorLocation2'] . '%)';
 			}
 
 			$is_on = array_column( (array) get_option( 'rbea_blocks' ), 'status', 'key' )['responsive-conditions'] ?? 1;
@@ -20099,7 +20099,8 @@ if ( ! class_exists( 'Responsive_Block_Editor_Addons_Frontend_Styles' ) ) {
 							: '',
 					'background-image'           =>
 						'gradient' === $attr['backgroundType']
-							? self::generate_background_image_effect(
+							? $attr['gradient'] ? $attr['gradient'] 
+							: self::generate_background_image_effect(
 								self::hex_to_rgb(
 									$attr['backgroundColor1'] ?? '#fff',
 									$imgopacity
