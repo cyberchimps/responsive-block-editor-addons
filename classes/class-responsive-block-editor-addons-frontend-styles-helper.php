e75781c3de287df12d681f64f75096ef4ba56588
Nikhil <sharma842nikhil@gmail.com>
2025-12-15 00:13:56 +0530

    removal of esc_textarea from frontend css as it breaks some of the css

--- classes/class-responsive-block-editor-addons-frontend-styles-helper.php
+++ classes/class-responsive-block-editor-addons-frontend-styles-helper.php
@@ -127,7 +127,7 @@ if ( ! class_exists( 'Responsive_Block_Editor_Addons_Frontend_Styles_Helper' ) )
     		// Combine post CSS and widget CSS and output it.
     		$combined_css = $post_css . $widget_css;
     		if ( ! empty( $combined_css ) ) {
-				echo '<style id="rbea-frontend-styles">' . esc_textarea( $combined_css ) . '</style>'; 
+				echo '<style id="rbea-frontend-styles">' . $combined_css . '</style>'; 
     		}
 			do_action( 'rbea-frontend-site-builder-styles' );
 		}
