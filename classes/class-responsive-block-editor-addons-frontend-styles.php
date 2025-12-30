7d5297ee42b95481a0f657cd122a4cb504957e9b
Nikhil <sharma842nikhil@gmail.com>
2025-12-30 10:21:58 +0530

    make button child text decoration default to none to match previous behavior

--- classes/class-responsive-block-editor-addons-frontend-styles.php
+++ classes/class-responsive-block-editor-addons-frontend-styles.php
@@ -4147,7 +4147,7 @@ if ( ! class_exists( 'Responsive_Block_Editor_Addons_Frontend_Styles' ) ) {
     			'hideWidgetMobile'		   => false,
 				'inheritFromThemesaved'	   => false,
 				'gradient'				   => '',
-				'buttonTextDecoration'	   => '',
+				'buttonTextDecoration'	   => 'none',
 			);
 		}
 
