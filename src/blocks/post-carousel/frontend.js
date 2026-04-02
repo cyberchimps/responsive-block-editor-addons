83206aa8ddd41656f01de00d93e4bd64ba483775
Prasada <prasada.pujari@hbwsl.com>
2026-04-02 12:52:44 +0530

    added bugfix for background color not working for post carousel when no featured image is available for any post

--- src/blocks/post-carousel/frontend.js
+++ src/blocks/post-carousel/frontend.js
@@ -1,4 +1,5 @@
 (function ($) {
+  console.log('in frontend.js====');
   $(window).on("load", function () {
     var wrap = jQuery(".responsive-block-editor-addons-block-post-carousel");
     var id;
@@ -7,8 +8,8 @@
 
       var scope = $(this).find(".responsive-post_carousel-equal-height-1");
       var post_active = scope.find(
-          ".responsive-block-editor-addons-post-carousel-inner"
-        ),
+        ".responsive-block-editor-addons-post-carousel-inner"
+      ),
         max_height = -1,
         post_active_height = -1,
         is_background_enabled = scope
@@ -16,14 +17,14 @@
           .hasClass("responsive-post__image-position-background");
       post_active.each(function (i) {
         var blog_post = $(this).find(
-            ".responsive-block-editor-addons-post-carousel-inner"
-          ),
+          ".responsive-block-editor-addons-post-carousel-inner"
+        ),
           blog_post_height = blog_post.outerHeight(),
           post_img_ht = $(this)
             .find(
               ".responsive-block-editor-addons-block-post-carousel-image-top"
             )
-            .outerHeight(),
+            .outerHeight() || 0,
           post_text_ht = $(this)
             .find(
               ".responsive-block-editor-addons-block-post-carousel-text-wrap"
@@ -35,11 +36,13 @@
         } else {
           blog_post_height = post_img_ht + post_text_ht;
         }
-
+        
         if (max_height < blog_post_height) {
           max_height = blog_post_height;
           post_active_height = max_height + 15;
+
         }
+        
       });
 
       post_active.each(function (i) {
